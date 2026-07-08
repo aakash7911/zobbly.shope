69f4f682e8e12192b4befac7f83162baf496755e
aakash <dr282924@gmail.com>
2026-07-09 02:47:17 +0530

    Fix GitHub Pages white screen by adding base path and HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { CartProvider } from './context/CartContext'
 import { AuthProvider } from './context/AuthContext'
 import { ProductProvider } from './context/ProductContext'
@@ -12,9 +12,9 @@ createRoot(document.getElementById('root')).render(
     <AuthProvider>
       <ProductProvider>
         <CartProvider>
-          <BrowserRouter>
+          <HashRouter>
             <App />
-          </BrowserRouter>
+          </HashRouter>
         </CartProvider>
       </ProductProvider>
     </AuthProvider>
