14ea501cd265d465fb6192f838c02758248b5b57
aakash <dr282924@gmail.com>
2026-07-09 14:50:33 +0530

    Change price to String in Product schema

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -4,7 +4,7 @@ const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
   brand: { type: String, required: true },
   condition: { type: String, required: true },
-  price: { type: Number, required: true },
+  price: { type: String, required: true },
   category: { type: String, required: true },
   image: { type: String, required: true }, // Cloudinary URL
   description: { type: String }
